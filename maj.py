84bbde2735114cc7bc80132b7868f14d0b2e25c3
Paul Nautré <66357224+loremipsumdsa@users.noreply.github.com>
2021-06-20 21:03:02 +0200

    jit correction

--- maj.py
+++ maj.py
@@ -4,15 +4,14 @@ import zipfile
 downloadF = os.system("wget -i configuration.txt")
 if downloadF:
 	print("Download fail (error "+ str(downloadF) + ")" )
-	return
+	exit()
 
 with zipfile.ZipFile("main.zip", 'r') as zip:
     zip.printdir()
     zip.extractall() 
 
-importF = os.system("scp -r main-site-perso panautre@perso.isima.fr:~public_html")
+importF = os.system("scp -r site-perso-main/* panautre@perso.isima.fr:~/public_html")
 if importF:
 	print("Download fail (error "+ str(importF) + ")" )
-	return
+	exit()
 print("Update success")
-
